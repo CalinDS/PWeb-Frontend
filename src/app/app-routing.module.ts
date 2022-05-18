bf31016e4df75a81e524247f2f465a71adca58cd
Lucian Roinita <roinitalucian@yahoo.com>
2022-05-19 00:33:23 +0300

    basic ui structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccommodationsComponent } from './accommodations/accommodations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accommodations', component: AccommodationsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { AccommodationsComponent } from './accommodations/accommodations.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccommodationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, RouterModule,
+    MatIconModule, MatToolbarModule, MatButtonModule, BrowserAnimationsModule, MatSidenavModule, MatMenuModule, MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
