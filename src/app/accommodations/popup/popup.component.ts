3ea12435688e446f25df2869c565baf37e623fd2
Lucian Roinita <roinitalucian@yahoo.com>
2022-05-21 17:02:02 +0300

    added support for booking accommodations

--- src/app/accommodations/popup/popup.component.ts
+++ src/app/accommodations/popup/popup.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import {Component, Inject, OnInit} from '@angular/core';
-import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-popup',
@@ -8,9 +10,35 @@ import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class PopupComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
+  public refugee_id: any;
+  public accommodation_id: any
+  authUser: any;
+  user: any;
+;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, public auth: AuthService, private http: HttpClient,
+    public dialogRef: MatDialogRef<PopupComponent>) { }
 
   ngOnInit(): void {
+    this.auth.user$.subscribe(
+      user => {
+        this.authUser = user;
+        this.http.get<any>('http://127.0.0.1:5000/users/' + user?.sub?.replace('|', '')).subscribe(data => {
+          this.user = data;
+          // console.log(this.user);
+        });
+      }
+    );
+  }
+
+  createBooking(): void {
+    const body = {
+      refugee_id: this.user.id,
+      accommodation_id: this.data.accommodation.id,
+    };
+    // console.log(body);
+    this.http.post<any>('http://127.0.0.1:5000/bookings/create', body).subscribe();
+    this.dialogRef.close();
   }
 
 }
